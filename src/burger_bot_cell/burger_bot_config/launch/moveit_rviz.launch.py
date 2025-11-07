5f4d1264a96006435ca8395cbb5d4fae68bd2e0b
jacob <jacobrawung1@hotmail.com>
2025-11-08 10:25:31 +1100

    successfully completed controll module

--- src/burger_bot_cell/burger_bot_config/launch/moveit_rviz.launch.py
+++ src/burger_bot_cell/burger_bot_config/launch/moveit_rviz.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_moveit_rviz_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("robot", package_name="robot_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("burger_bot_cell", package_name="burger_bot_config").to_moveit_configs()
     return generate_moveit_rviz_launch(moveit_config)
